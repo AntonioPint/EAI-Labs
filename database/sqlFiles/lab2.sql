62f4988829663ff79630f4b8b686e87444d7c36d
António Pinto <180221026@estudantes.ips.pt>
2024-04-12 10:55:33 +0100

    Update lab2.sql

--- database/sqlFiles/lab2.sql
+++ database/sqlFiles/lab2.sql
@@ -1,8 +1,7 @@
-use eai;
-
+-- use eai;
+use lojacarl_EAI_HotelReviews_Lab;
 -- ALTER TABLE hotel_review
 -- ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY FIRST;
-
 
 drop table training_set ;
 create table training_set (
@@ -29,63 +28,85 @@ select count(*) from hotel_review where Reviewer_Score < 3;
 select count(*) from hotel_review where Reviewer_Score > 8;
 
 
-INSERT INTO training_set (review_id, class)
-SELECT ht.id, 0
-FROM hotel_review ht
-WHERE ht.Reviewer_Score < 3
-AND ht.id NOT IN (
-    SELECT result.review_id 
-    FROM (
-        SELECT review_id FROM testing_set 
-        UNION 
-        SELECT review_id FROM training_set
-    ) result
-)
-LIMIT 800;
+insert into training_set (review_id, class)
+select * from (
+	SELECT ht.id, 0
+	FROM hotel_review ht
+	WHERE ht.Reviewer_Score < 3
+	AND ht.id NOT IN (
+		SELECT result.review_id 
+		FROM (
+			SELECT review_id FROM testing_set 
+			UNION 
+			SELECT review_id FROM training_set
+		) result
+	)
+	order by ht.Reviewer_Score asc
+	LIMIT 1000
+) as subquery 
+order by rand() 
+limit 800;
 
-INSERT INTO training_set (review_id, class)
-SELECT ht.id, 1
-FROM hotel_review ht
-WHERE ht.Reviewer_Score >= 8
-AND ht.id NOT IN (
-    SELECT result.review_id 
-    FROM (
-        SELECT review_id FROM testing_set 
-        UNION 
-        SELECT review_id FROM training_set
-    ) result
-)
-LIMIT 800;
+insert into testing_set (review_id, class)
+select * from (
+	SELECT ht.id, 0
+	FROM hotel_review ht
+	WHERE ht.Reviewer_Score < 3
+	AND ht.id NOT IN (
+		SELECT result.review_id 
+		FROM (
+			SELECT review_id FROM testing_set 
+			UNION 
+			SELECT review_id FROM training_set
+		) result
+	)
+	order by ht.Reviewer_Score asc
+	LIMIT 1000
+) as subquery 
+order by rand() 
+limit 200;
+
 -------------------------------------------------
-INSERT INTO testing_set (review_id, class)
-SELECT ht.id, 0
-FROM hotel_review ht
-WHERE ht.Reviewer_Score < 3 
-AND ht.id NOT IN (
-    SELECT result.review_id 
-    FROM (
-        SELECT review_id FROM testing_set 
-        UNION 
-        SELECT review_id FROM training_set
-    ) result
-)
-LIMIT 200;
+insert into training_set (review_id, class)
+select * from (
+	SELECT ht.id, 1
+	FROM hotel_review ht
+	WHERE ht.Reviewer_Score >= 8
+	AND ht.id NOT IN (
+		SELECT result.review_id 
+		FROM (
+			SELECT review_id FROM testing_set 
+			UNION 
+			SELECT review_id FROM training_set
+		) result
+	)
+	order by ht.Reviewer_Score asc
+	LIMIT 1000
+) as subquery 
+order by rand() 
+limit 800;
 
-INSERT INTO testing_set (review_id, class)
-SELECT ht.id, 1
-FROM hotel_review ht
-WHERE ht.Reviewer_Score >= 8 
-AND ht.id NOT IN (
-    SELECT result.review_id 
-    FROM (
-        SELECT review_id FROM testing_set 
-        UNION 
-        SELECT review_id FROM training_set
-    ) result
-)
-LIMIT 200;
+insert into testing_set (review_id, class)
+select * from (
+	SELECT ht.id, 1
+	FROM hotel_review ht
+	WHERE ht.Reviewer_Score >= 8
+	AND ht.id NOT IN (
+		SELECT result.review_id 
+		FROM (
+			SELECT review_id FROM testing_set 
+			UNION 
+			SELECT review_id FROM training_set
+		) result
+	)
+	order by ht.Reviewer_Score asc
+	LIMIT 1000
+) as subquery 
+order by rand() 
+limit 200;
 
-select * from training_set tas inner join testing_set tes on tes.review_id = tas.review_id
+
+select * from training_set tas inner join testing_set tes on tes.review_id = tas.review_id -- should be empty
 
 -- select hr.Negative_Review FROM hotel_review hr inner join training_set ts on hr.id = ts.review_id where ts.class = 1
 
