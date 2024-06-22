1e80e9de26ccf16ef7373516ebdb0fbcfc2f9cb5
Guilherme Malhado <74466854+guilhermemalhado1@users.noreply.github.com>
2024-06-22 16:24:26 +0100

    added cache on cossinesmilarity promises return object

--- classification/classifier.js
+++ classification/classifier.js
@@ -1,67 +1,77 @@
-const counting = require("./counting.js")
-const preprocessing = require("./preprocessing.js")
+const counting = require("./counting.js");
+const preprocessing = require("./preprocessing.js");
 const termRepository = require("../database/repositories/termRepository.js");
 const termStatisticRepository = require("../database/repositories/termStatisticRepository.js");
 
+// Default initialization of cache
+const cache = {
+    termsOriginalNegative: null,
+    termsOriginalPositive: null,
+    termsStatistics: null,
+    populated: false
+};
+
 async function cossineSimilarityResult(text) {
-    let preProcessed = preprocessing(text, [1, 2])
+    let preProcessed = preprocessing(text, [1, 2]);
 
     preProcessed.tf = preProcessed.tokens.map(tokensNgram =>
         tokensNgram.map(token => counting.tf(tokensNgram, token))
     );
-    preProcessed.tokens = [].concat(...preProcessed.tokens)
-    preProcessed.tf = [].concat(...preProcessed.tf)
-    preProcessed.tfidfPositive = []
-    preProcessed.tfidfNegative = []
+    preProcessed.tokens = [].concat(...preProcessed.tokens);
+    preProcessed.tf = [].concat(...preProcessed.tf);
+    preProcessed.tfidfPositive = [];
+    preProcessed.tfidfNegative = [];
 
-    let [termsOriginalNegative, termsOriginalPositive, termsStatistics] = await Promise.all([
-        termRepository.getIdfOfTerms(0),
-        termRepository.getIdfOfTerms(1),
-        termStatisticRepository.getTermStatisticsFormated()
-    ]);
+    if (!cache.populated) {
+        [cache.termsOriginalNegative, cache.termsOriginalPositive, cache.termsStatistics] = await Promise.all([
+            termRepository.getIdfOfTerms(0),
+            termRepository.getIdfOfTerms(1),
+            termStatisticRepository.getTermStatisticsFormated()
+        ]);
 
-    for (let termStatistic of termsStatistics) {
-        termStatistic.bagOfWords = JSON.parse(termStatistic.bagOfWords)
+        for (let termStatistic of cache.termsStatistics) {
+            termStatistic.bagOfWords = JSON.parse(termStatistic.bagOfWords);
+        }
+
+        cache.populated = true;
     }
 
-    let termPositivesList = termsStatistics.find(term => term.label === 1)?.bagOfWords || [];
-
-    let termNegativesList = termsStatistics.find(term => term.label === 0)?.bagOfWords || [];
-
+    let termPositivesList = cache.termsStatistics.find(term => term.label === 1)?.bagOfWords || [];
+    let termNegativesList = cache.termsStatistics.find(term => term.label === 0)?.bagOfWords || [];
 
     const termPositives = new Map(termPositivesList.map(e => [e.name, { tf: e.tf, tfidf: e.tfidf, classification: e.classification }]));
     const termNegatives = new Map(termNegativesList.map(e => [e.name, { tf: e.tf, tfidf: e.tfidf, classification: e.classification }]));
 
     for (let i = 0; i < preProcessed.tokens.length; i++) {
-        let wordIdfPositive = termsOriginalPositive.get(preProcessed.tokens[i])?.idf || 0
-        let wordIdfNegative = termsOriginalNegative.get(preProcessed.tokens[i])?.idf || 0
+        let wordIdfPositive = cache.termsOriginalPositive.get(preProcessed.tokens[i])?.idf || 0;
+        let wordIdfNegative = cache.termsOriginalNegative.get(preProcessed.tokens[i])?.idf || 0;
 
         preProcessed.tfidfPositive.push(
             counting.tfidf(
                 preProcessed.tf[i] || 0,
                 wordIdfPositive
             )
-        )
+        );
         preProcessed.tfidfNegative.push(
             counting.tfidf(
                 preProcessed.tf[i] || 0,
                 wordIdfNegative
             )
-        )
+        );
     }
 
-    let originalTfidfPositive = preProcessed.tokens.map(e => { return termPositives.get(e)?.tfidf || 0 })
-    let originalTfidfNegative = preProcessed.tokens.map(e => { return termNegatives.get(e)?.tfidf || 0 })
+    let originalTfidfPositive = preProcessed.tokens.map(e => { return termPositives.get(e)?.tfidf || 0 });
+    let originalTfidfNegative = preProcessed.tokens.map(e => { return termNegatives.get(e)?.tfidf || 0 });
 
-    let similarityCossenPositive = cosineSimilarity(preProcessed.tfidfPositive, originalTfidfPositive)
-    let similarityCossenNegative = cosineSimilarity(preProcessed.tfidfNegative, originalTfidfNegative)
+    let similarityCossenPositive = cosineSimilarity(preProcessed.tfidfPositive, originalTfidfPositive);
+    let similarityCossenNegative = cosineSimilarity(preProcessed.tfidfNegative, originalTfidfNegative);
 
-    let decision = +(similarityCossenPositive > similarityCossenNegative)
+    let decision = +(similarityCossenPositive > similarityCossenNegative);
 
     return {
-        decision: decision, 
+        decision: decision,
         similarityCossenPositiveNegative: [similarityCossenPositive, similarityCossenNegative]
-    }
+    };
 }
 
 function cosineSimilarity(vector1, vector2) {
@@ -87,13 +97,12 @@ function cosineSimilarity(vector1, vector2) {
     magnitude2 = Math.sqrt(magnitude2);
 
     // Calculate cosine similarity
-    if ((magnitude1 * magnitude2) == 0) return 0
+    if ((magnitude1 * magnitude2) == 0) return 0;
     let similarity = dotProduct / (magnitude1 * magnitude2);
 
     return similarity;
 }
 
-
-module.exports = { 
-    cossineSimilarityResult: cossineSimilarityResult 
-}
+module.exports = {
+    cossineSimilarityResult: cossineSimilarityResult
+};
